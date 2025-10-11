aa66fb1530562221f397d5b6d401e552c52b6325
Aminudin Abdulloh <aminabdulah01@gmail.com>
2025-10-12 00:55:03 +0700

    feat: Menampilkan media partner di halaman informasi (public)

--- src/controllers/InformationController.php
+++ src/controllers/InformationController.php
@@ -1,9 +1,18 @@
 <?php
 
 class InformationController extends BaseController{
+    private $mediaPartnerModel;
+
+    public function __construct() {
+        $this->mediaPartnerModel = $this->model('MediaPartnerModel');
+    }
+
     public function index() {
+        $media_partners = $this->mediaPartnerModel->getAll();
+
         $data = [
             'title' => 'Informations',
+            'media_partners' => $media_partners,
         ];
 
         $this->view('templates/public/header', $data);
--- src/views/public/information/index.php
+++ src/views/public/information/index.php
@@ -1,6 +1,14 @@
 <div class="information-section">
     <h1>MEDIA PARTNER</h1>
     <div class="media-partner">
-        
+        <?php if(!empty($media_partners)): ?>
+            <?php foreach($media_partners as $partner): ?>
+                <div class="logo-card">
+                    <img src="<?= BASEURL . '/' . htmlspecialchars($partner['logo']); ?>" alt="<?php echo htmlspecialchars($partner['name']); ?>">
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <p>Tidak ada media partner yang tersedia.</p>
+        <?php endif; ?>
     </div>
 </div>
